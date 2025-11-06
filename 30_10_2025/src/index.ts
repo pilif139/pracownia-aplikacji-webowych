94ff9bf5646b4e87eb90d1341526e7862ec4db52
pilif139 <pifilif@gmail.com>
2025-11-06 10:20:33 +0100

    remove bun.lock

--- 30_10_2025/src/index.ts
+++ 30_10_2025/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import type { Express } from 'express'
 import router from './routing'
 import path from 'path'
-import getApiRouter from './api.route'
+import getApiRouter from './api'
 
 const app: Express = express()
 
@@ -10,10 +10,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.use('/', router)
-// ;(async () => {
-//   const apiRouter = await getApiRouter()
-//   app.use('/', apiRouter)
-// })()
+;(async () => {
+  const apiRouter = await getApiRouter()
+  app.use('/', apiRouter)
+})()
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
